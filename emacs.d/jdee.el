625d069a7276e0a879a634dabf35f0e73db0fb00
Bryan Shell <github@ennor.co.uk>
2010-10-04 10:46:59 -0500

    Merge jdee config with work

--- emacs.d/jdee.el
+++ emacs.d/jdee.el
@@ -2,17 +2,25 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/packages/jdee-extra"))
 
 (autoload 'jde-mode "jde" "JDE mode." t)
-
-;(semantic-mode)
+(autoload 'semantic-mode "semantic" "Semantic mode." t)
 
 (setq auto-mode-alist (append '(("\\.java\\'" . jde-mode)) auto-mode-alist))
 
-(set-default  'jde-jdk-registry (quote (("1.4" . "/usr/local/java/jdk1.4")
-					("1.5" . "/usr/local/java/jdk1.5")
-					("1.6" . "/usr/local/java/jdk1.6")
-					("1.3" . "/usr/local/java/jdk1.3"))))
-(set-default 'jde-jdk (quote ("1.6")))
+(eval-after-load 'jde
+  '(progn
+     (semantic-mode 1)
+     (set-default  'jde-jdk-registry (quote (("1.4" . "/usr/local/java/jdk1.4")
+                                             ("1.5" . "/usr/local/java/jdk1.5")
+                                             ("1.6" . "/usr/local/java/jdk1.6")
+                                             ("1.3" . "/usr/local/java/jdk1.3"))))
+     (set-default 'jde-jdk (quote ("1.6")))
+     (setq semantic-new-buffer-setup-functions (append '((jde-mode . wisent-java-default-setup)) semantic-new-buffer-setup-functions))))
 
-;(setq semantic-new-buffer-setup-functions (append '((jde-mode . wisent-java-default-setup)) semantic-new-buffer-setup-functions))
+(eval-after-load 'jde-complete
+  '(progn
+     (set-default 'jde-complete-insert-method-signature nil)))
 
-
+(eval-after-load 'jde-help
+  '(progn
+     (add-to-list 'jde-help-docsets '(nil "http://download.oracle.com/docs/cd/E17802_01/j2se/javase/technologies/desktop/javabeans/glasgow/javadocs/" nil)) ;; JavaMail
+     (add-to-list 'jde-help-docsets '(nil "http://hadoop.apache.org/common/docs/r0.20.2/api/" nil)))) ;; Hadoop
