8d8541e3706f32988bfd5ce19199cfa90b2f89e9
Bryan Shell <github@ennor.co.uk>
2010-10-12 18:22:35 -0500

    Removed unneeded color changes.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -7,12 +7,6 @@
 (load-file (expand-file-name "~/.emacs.d/myvc.el"))
 (load-file (expand-file-name "~/.emacs.d/mytramp.el"))
 
-(set-background-color "black")
-(set-face-background 'region "black")
-(set-face-foreground 'default "white")
-(set-face-foreground 'region "gray60")
-(set-foreground-color "white")
-
 (when (fboundp 'global-font-lock-mode)
   (global-font-lock-mode t))
 
