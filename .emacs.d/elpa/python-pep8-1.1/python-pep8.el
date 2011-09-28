c1ab5cfe857a7890f57a13b9bee3216bcad29fdb
Bryan Shell <github@ennor.co.uk>
2011-09-27 23:46:41 -0500

    Pulled python-pep8 and python-pylint packages from Marmalade

--- .emacs.d/elpa/python-pep8-1.1/python-pep8.el
+++ .emacs.d/elpa/python-pep8-1.1/python-pep8.el
@@ -1,4 +1,4 @@
-;;; python-pylint.el --- minor mode for running `pylint'
+;;; python-pep8.el --- minor mode for running `pep8'
 
 ;; Copyright (c) 2009, 2010 Ian Eure <ian.eure@gmail.com>
 
@@ -8,7 +8,7 @@
 ;; Last edit: 2010-02-12
 ;; Version: 1.01
 
-;; python-pylint.el is free software; you can redistribute it and/or modify it
+;; python-pep8.el is free software; you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by the Free
 ;; Software Foundation; either version 2, or (at your option) any later
 ;; version.
@@ -25,53 +25,51 @@
 
 ;;; Commentary:
 ;;
-;; (autoload 'python-pylint "python-pylint")
-;; (autoload 'pylint "python-pylint")
+;; (autoload 'python-pep8 "python-pep8")
+;; (autoload 'pep8 "python-pep8")
 ;;
 ;;; Code:
-(require 'tramp)
 
-(defgroup python-pylint nil
-  "Minor mode for running pylint"
-  :prefix "python-pylint-"
+(defgroup python-pep8 nil
+  "Minor mode for running pep8"
+  :prefix "python-pep8-"
   :group 'tools)
 
-(defvar python-pylint-last-buffer nil
-  "The most recent PYLINT buffer.
-A PYLINT buffer becomes most recent when you select PYLINT mode in it.
+(defvar python-pep8-last-buffer nil
+  "The most recent PEP8 buffer.
+A PEP8 buffer becomes most recent when you select PEP8 mode in it.
 Notice that using \\[next-error] or \\[compile-goto-error] modifies
-`complation-last-buffer' rather than `python-pylint-last-buffer'.")
+`complation-last-buffer' rather than `python-pep8-last-buffer'.")
 
-(defconst python-pylint-regexp-alist
-  (let ((base "^\\(.*\\):\\([0-9]+\\):\s+\\(\\[%s.*\\)$"))
-    (list
-     (list (format base "[FE]") 1 2)
-     (list (format base "[RWC]") 1 2 nil 1)))
-  "Regexp used to match PYLINT hits.  See `compilation-error-regexp-alist'.")
+(defconst python-pep8-regexp-alist
+  (let ((base "^\\(.*\\):\\([0-9]+\\):\\([0-9]+\\):\s+\\(%s[0-9]+ .*\\)"))
+    (list (list (format base "E") 1 2 3 2)
+          (list (format base "W") 1 2 3 1)))
+  "Regexp used to match PEP8 hits.  See `compilation-error-regexp-alist'.")
 
-(defcustom python-pylint-options '("-rn" "-f parseable")
-  "Options to pass to pylint.py"
+(defcustom python-pep8-options '("--repeat")
+  "Options to pass to pep8.py"
   :type '(repeat string)
-  :group 'python-pylint)
+  :group 'python-pep8)
 
-(defcustom python-pylint-command "pylint"
-  "PYLINT command."
+(defcustom python-pep8-command "pep8"
+  "PEP8 command."
   :type '(file)
-  :group 'python-pylint)
+  :group 'python-pep8)
 
-(defcustom python-pylint-ask-about-save nil
-  "Non-nil means \\[python-pylint] asks which buffers to save before compiling.
+(defcustom python-pep8-ask-about-save nil
+  "Non-nil means \\[python-pep8] asks which buffers to save before compiling.
 Otherwise, it saves all modified buffers without asking."
   :type 'boolean
-  :group 'python-pylint)
+  :group 'python-pep8)
 
-(define-compilation-mode python-pylint-mode "PYLINT"
-  (setq python-pylint-last-buffer (current-buffer))
+(define-compilation-mode python-pep8-mode "PEP8"
+  (setq python-pep8-last-buffer (current-buffer))
   (set (make-local-variable 'compilation-error-regexp-alist)
-       python-pylint-regexp-alist)
+       python-pep8-regexp-alist)
   (set (make-local-variable 'compilation-disable-input) t))
 
-(defvar python-pylint-mode-map
+(defvar python-pep8-mode-map
   (let ((map (make-sparse-keymap)))
     (set-keymap-parent map compilation-minor-mode-map)
     (define-key map " " 'scroll-up)
@@ -86,31 +84,33 @@ Otherwise, it saves all modified buffers without asking."
     (define-key map "\t" 'compilation-next-error)
     (define-key map [backtab] 'compilation-previous-error)
     map)
-  "Keymap for PYLINT buffers.
+  "Keymap for PEP8 buffers.
 `compilation-minor-mode-map' is a cdr of this.")
 
 ;;;###autoload
-(defun python-pylint ()
-  "Run PYLINT, and collect output in a buffer.
-While pylint runs asynchronously, you can use \\[next-error] (M-x next-error),
-or \\<python-pylint-mode-map>\\[compile-goto-error] in the grep \
-output buffer, to go to the lines where pylint found matches."
+(defun python-pep8 ()
+  "Run PEP8, and collect output in a buffer.
+While pep8 runs asynchronously, you can use \\[next-error] (M-x next-error),
+or \\<python-pep8-mode-map>\\[compile-goto-error] in the grep \
+output buffer, to go to the lines where pep8 found matches."
   (interactive)
 
-  (save-some-buffers (not python-pylint-ask-about-save) nil)
+  (save-some-buffers (not python-pep8-ask-about-save) nil)
   (let* ((tramp (tramp-tramp-file-p (buffer-file-name)))
          (file (or (and tramp
                         (aref (tramp-dissect-file-name (buffer-file-name)) 3))
                    (buffer-file-name)))
          (command (mapconcat
                    'identity
-                   (list python-pylint-command
-                         (mapconcat 'identity python-pylint-options " ")
-                         (comint-quote-filename file)) " ")))
+                   (list python-pep8-command
+                         (mapconcat 'identity python-pep8-options " ")
+                         (comint-quote-filename file)
+                         "|" "sort" "-n" "-t:" "-k2") " ")))
 
-    (compilation-start command 'python-pylint-mode)))
+    (compilation-start command 'python-pep8-mode)))
 
 ;;;###autoload
-(defalias 'pylint 'python-pylint)
+(defalias 'pep8 'python-pep8)
 
-(provide 'python-pylint)
+(provide 'python-pep8)
+;;; python-pep8.el ends here
--- .emacs.d/elpa/python-pylint-1.1/python-pylint.el
+++ .emacs.d/elpa/python-pylint-1.1/python-pylint.el
@@ -1,4 +1,4 @@
-;;; python-pep8.el --- minor mode for running `pep8'
+;;; python-pylint.el --- minor mode for running `pylint'
 
 ;; Copyright (c) 2009, 2010 Ian Eure <ian.eure@gmail.com>
 
@@ -8,7 +8,7 @@
 ;; Last edit: 2010-02-12
 ;; Version: 1.01
 
-;; python-pep8.el is free software; you can redistribute it and/or modify it
+;; python-pylint.el is free software; you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by the Free
 ;; Software Foundation; either version 2, or (at your option) any later
 ;; version.
@@ -25,51 +25,52 @@
 
 ;;; Commentary:
 ;;
-;; (autoload 'python-pep8 "python-pep8")
-;; (autoload 'pep8 "python-pep8")
+;; (autoload 'python-pylint "python-pylint")
+;; (autoload 'pylint "python-pylint")
 ;;
 ;;; Code:
 
-(defgroup python-pep8 nil
-  "Minor mode for running pep8"
-  :prefix "python-pep8-"
+(defgroup python-pylint nil
+  "Minor mode for running pylint"
+  :prefix "python-pylint-"
   :group 'tools)
 
-(defvar python-pep8-last-buffer nil
-  "The most recent PEP8 buffer.
-A PEP8 buffer becomes most recent when you select PEP8 mode in it.
+(defvar python-pylint-last-buffer nil
+  "The most recent PYLINT buffer.
+A PYLINT buffer becomes most recent when you select PYLINT mode in it.
 Notice that using \\[next-error] or \\[compile-goto-error] modifies
-`complation-last-buffer' rather than `python-pep8-last-buffer'.")
+`complation-last-buffer' rather than `python-pylint-last-buffer'.")
 
-(defconst python-pep8-regexp-alist
-  (let ((base "^\\(.*\\):\\([0-9]+\\):\\([0-9]+\\):\s+\\(%s[0-9]+ .*\\)"))
-    (list (list (format base "E") 1 2 3 2)
-          (list (format base "W") 1 2 3 1)))
-  "Regexp used to match PEP8 hits.  See `compilation-error-regexp-alist'.")
+(defconst python-pylint-regexp-alist
+  (let ((base "^\\(.*\\):\\([0-9]+\\):\s+\\(\\[%s.*\\)$"))
+    (list
+     (list (format base "[FE]") 1 2)
+     (list (format base "[RWC]") 1 2 nil 1)))
+  "Regexp used to match PYLINT hits.  See `compilation-error-regexp-alist'.")
 
-(defcustom python-pep8-options '("--repeat")
-  "Options to pass to pep8.py"
+(defcustom python-pylint-options '("-rn" "-f parseable")
+  "Options to pass to pylint.py"
   :type '(repeat string)
-  :group 'python-pep8)
+  :group 'python-pylint)
 
-(defcustom python-pep8-command "pep8"
-  "PEP8 command."
+(defcustom python-pylint-command "pylint"
+  "PYLINT command."
   :type '(file)
-  :group 'python-pep8)
+  :group 'python-pylint)
 
-(defcustom python-pep8-ask-about-save nil
-  "Non-nil means \\[python-pep8] asks which buffers to save before compiling.
+(defcustom python-pylint-ask-about-save nil
+  "Non-nil means \\[python-pylint] asks which buffers to save before compiling.
 Otherwise, it saves all modified buffers without asking."
   :type 'boolean
-  :group 'python-pep8)
+  :group 'python-pylint)
 
-(define-compilation-mode python-pep8-mode "PEP8"
-  (setq python-pep8-last-buffer (current-buffer))
+(define-compilation-mode python-pylint-mode "PYLINT"
+  (setq python-pylint-last-buffer (current-buffer))
   (set (make-local-variable 'compilation-error-regexp-alist)
-       python-pep8-regexp-alist)
+       python-pylint-regexp-alist)
   (set (make-local-variable 'compilation-disable-input) t))
 
-(defvar python-pep8-mode-map
+(defvar python-pylint-mode-map
   (let ((map (make-sparse-keymap)))
     (set-keymap-parent map compilation-minor-mode-map)
     (define-key map " " 'scroll-up)
@@ -84,32 +85,32 @@ Otherwise, it saves all modified buffers without asking."
     (define-key map "\t" 'compilation-next-error)
     (define-key map [backtab] 'compilation-previous-error)
     map)
-  "Keymap for PEP8 buffers.
+  "Keymap for PYLINT buffers.
 `compilation-minor-mode-map' is a cdr of this.")
 
 ;;;###autoload
-(defun python-pep8 ()
-  "Run PEP8, and collect output in a buffer.
-While pep8 runs asynchronously, you can use \\[next-error] (M-x next-error),
-or \\<python-pep8-mode-map>\\[compile-goto-error] in the grep \
-output buffer, to go to the lines where pep8 found matches."
+(defun python-pylint ()
+  "Run PYLINT, and collect output in a buffer.
+While pylint runs asynchronously, you can use \\[next-error] (M-x next-error),
+or \\<python-pylint-mode-map>\\[compile-goto-error] in the grep \
+output buffer, to go to the lines where pylint found matches."
   (interactive)
 
-  (save-some-buffers (not python-pep8-ask-about-save) nil)
+  (save-some-buffers (not python-pylint-ask-about-save) nil)
   (let* ((tramp (tramp-tramp-file-p (buffer-file-name)))
          (file (or (and tramp
                         (aref (tramp-dissect-file-name (buffer-file-name)) 3))
                    (buffer-file-name)))
          (command (mapconcat
                    'identity
-                   (list python-pep8-command
-                         (mapconcat 'identity python-pep8-options " ")
-                         (comint-quote-filename file)
-                         "|" "sort" "-n" "-t:" "-k2") " ")))
+                   (list python-pylint-command
+                         (mapconcat 'identity python-pylint-options " ")
+                         (comint-quote-filename file)) " ")))
 
-    (compilation-start command 'python-pep8-mode)))
+    (compilation-start command 'python-pylint-mode)))
 
 ;;;###autoload
-(defalias 'pep8 'python-pep8)
+(defalias 'pylint 'python-pylint)
 
-(provide 'python-pep8)
+(provide 'python-pylint)
+;;; python-pylint.el ends here
