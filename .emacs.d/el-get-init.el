1a739b14e90bab6409715f016cca88578d431505
Bryan Shell <bryan.shell@orbitz.com>
2013-01-11 09:46:16 -0600

    Update el-get init to work on a clean install

--- .emacs.d/el-get-init.el
+++ .emacs.d/el-get-init.el
@@ -47,22 +47,21 @@
   (if (string-equal pkg "el-get")
       (let ((my-packages (append el-get-packages
                                  (mapcar 'el-get-source-name el-get-sources))))
+	;; Add locally curated recipes for el-get
+	(add-to-list 'el-get-recipe-path (file-name-directory el-get-user-package-directory))
+
         (el-get-cleanup my-packages)
         (el-get 'sync my-packages))))
 (add-hook 'el-get-post-install-hooks 'my-el-get-post-install-hook)
 
 ;; try to require el-get
-(unless (require 'el-get nil t)
+(unless (require 'el-get nil 'noerror)
   (with-current-buffer
       (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
-       (lambda (s)
-         (let (el-get-master-branch)
-           (end-of-buffer)
-           (eval-print-last-sexp))))))
-
-;; Add locally curated recipes for el-get
-(add-to-list 'el-get-recipe-path (file-name-directory el-get-user-package-directory))
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
 
 ;; successfully required el-get, load the packages!
 (my-el-get-post-install-hook "el-get")
