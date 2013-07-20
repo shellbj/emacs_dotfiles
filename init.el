229c56835319c88c43f7e93e530dcc6c957f9789
Bryan Shell <bryan.shell@orbitz.com>
2013-07-20 15:29:30 -0500

    Reorganize and comment my settings

--- init.el
+++ init.el
@@ -1,14 +1,72 @@
+;; random settings
+(setq
+ c-basic-offset 4
+ diff-switches "-u"
+ ; contemplate crass confusion
+ enable-recursive-minibuffers t)
+
+;;; el-get
 (add-to-list 'load-path (expand-file-name "el-get/el-get" user-emacs-directory))
 (load-file (expand-file-name "el-get-init.el" user-emacs-directory))
 
+;;; User info
+(setq user-full-name "Bryan Shell")
+(setq user-mail-address "bryan.shell@orbitz.com")
+
+;;; GUI Settings
+(setq
+ ;; Do not show a splash screen.
+ inhibit-splash-screen t
+ ;; Do not show the startup message
+ inhibit-startup-message t
+ ;; Prevent mouse acrobatics
+ mouse-yank-at-point t
+ ;; Show incomplete chords while typing them.
+ echo-keystrokes 0.1)
+
+(setq-default
+ ;; Show start and end of buffers on the right
+ indicate-buffer-boundaries 'right
+ ;; Show the end of files inside buffers.
+ indicate-empty-lines t)
+
+;; enable visual feedback on selections
+(transient-mark-mode t)
+;; delete selected text
+(delete-selection-mode t)
+
+;; font-lock everything
 (when (fboundp 'global-font-lock-mode)
   (global-font-lock-mode t))
 
-(set-default 'indent-tabs-mode nil)
+;; Revert buffers when underlying files change
+(global-auto-revert-mode t)
 
+;; Window configuration undo/redo
+(winner-mode t)
+
+;; Move around windows with arrow keys
+(windmove-default-keybindings 'shift)
+(global-set-key (kbd "C-c <left>") 'windmove-left)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
+(global-set-key (kbd "C-c <up>") 'windmove-up)
+(global-set-key (kbd "C-c <down>") 'windmove-down)
+
+;; add a new line to the end on saves
+(setq require-final-newline t)
+
+;; tab handling
+(set-default indent-tabs-mode nil
+             tab-width 2)
+
+;; Always show matching sets of parentheses
 (show-paren-mode t)
 (setq show-paren-style 'parentheses)
 
+;; Highlight the current line.
+(global-hl-line-mode t)
+
+;; re-enable commands
 (put 'set-goal-column 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (put 'scroll-left 'disabled nil)
@@ -17,38 +75,44 @@
 (put 'erase-buffer 'disabled nil)
 (put 'dired-find-alternate-file 'disabled nil)
 
+;; alias "yes" or "no" to "y" or "n"
 (fset 'yes-or-no-p 'y-or-n-p)
 
-(setq
- transient-mark-mode t ;; enable visual feedback on selections
- c-basic-offset 4
- mouse-yank-at-point t
- inhibit-startup-message t
- backup-by-copying-when-linked t
- auto-save-interval 600
- version-control t
- indicate-empty-lines t
- indicate-buffer-boundaries (quote ((top . left) (bottom . right)))
- diff-switches "-u"
- display-time-24hr-format t
- add-log-always-start-new-record t
- mode-line-in-non-selected-windows nil  ; keep things quiet for now
- enable-recursive-minibuffers t         ; contemplate crass confusion
- confirm-kill-emacs 'y-or-n-p)
-
-(menu-bar-mode 't)
-(set-scroll-bar-mode nil)
+;; Hide the scroll bar, tool bar, and menu bar.
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
 (tool-bar-mode -1)
-(show-paren-mode t)
-(column-number-mode t)
+
+;; No Rudolf to light my way
 (blink-cursor-mode -1)
 
+;;; Modeline info
+;; Line and Column number in the modeline
+(line-number-mode t)
+(column-number-mode t)
+
+ ;; show the clock in the proper format
+(setq display-time-24hr-format t)
+(display-time-mode t)
+
+;;; Backups
+(setq auto-save-file-name-transforms `(("." ,(expand-file-name "backups" user-emacs-directory) t)))
 (add-to-list 'backup-directory-alist (cons tramp-file-name-regexp nil))
 (add-to-list 'backup-directory-alist (cons "." (expand-file-name "backups" user-emacs-directory)))
 
+(setq
+ ;; always make numbered backups
+ version-control t
+ ;; always copy for backups
+ backup-by-copying t
+ ;; handle backups for linked files properly
+ backup-by-copying-when-linked t)
+
+;;; Themes
 (setq custom-theme-directory (expand-file-name "themes" user-emacs-directory))
 (load-theme 'my t)
 
+;;; Dired
 (defun my-dired-load-hook ()
   (require 'dired-x))
 (add-hook 'dired-load-hook 'my-dired-load-hook)
@@ -59,18 +123,5 @@
   (define-key dired-mode-map "\C-k" 'dired-kill-subdir))
 (add-hook 'dired-mode-hook 'my-dired-mode-hook)
 
-;; only delete the selection on a delete command
-(require 'delsel)
-(eval-after-load "delsel"
-  (progn
-    (put 'self-insert-command 'delete-selection nil)
-    (put 'self-insert-iso 'delete-selection nil)
-    (put 'yank 'delete-selection nil)
-    (put 'clipboard-yank 'delete-selection nil)
-    (put 'insert-register 'delete-selection nil)
-    (put 'newline-and-indent 'delete-selection nil)
-    (put 'newline 'delete-selection nil)
-    (put 'open-line 'delete-selection nil)))
-
-;; zsh automode
+;;; zsh automode
 (add-to-list 'auto-mode-alist '("\\.zsh" . sh-mode))
