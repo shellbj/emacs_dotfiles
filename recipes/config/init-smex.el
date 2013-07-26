845a0740019358ca08bddd7fc652538041e2cb5a
Bryan Shell <bryan.shell@orbitz.com>
2013-07-26 10:32:38 -0500

    Move smex history into .emacs.d

--- recipes/config/init-smex.el
+++ recipes/config/init-smex.el
@@ -1,2 +1,3 @@
+(setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
 (define-key (current-global-map) [remap execute-extended-command] 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
