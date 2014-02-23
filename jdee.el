abb6646a77ddc86d86d3baa399794b742c8e5511
Bryan Shell <bryan.shell@orbitz.com>
2014-02-23 15:07:55 -0600

    JDEE: setup for jenkins plugin work.

--- jdee.el
+++ jdee.el
@@ -7,11 +7,16 @@
 
 (eval-after-load 'jde
   '(progn
+     (require 'jde-maven)
+     (set-default 'jde-complete-function 'jde-complete-minibuf)
+     (setq semantic-default-submodes '(global-semantic-idle-scheduler-mode
+                                       global-semanticdb-minor-mode
+                                       global-semantic-idle-summary-mode
+                                       global-semantic-mru-bookmark-mode))
      (semantic-mode 1)
-     (set-default  'jde-jdk-registry (quote (("1.4" . "/usr/local/java/jdk1.4")
-                                             ("1.5" . "/usr/local/java/jdk1.5")
-                                             ("1.6" . "/usr/local/java/jdk1.6")
-                                             ("1.3" . "/usr/local/java/jdk1.3"))))
+     (set-default  'jde-jdk-registry (quote (("1.4" . "/opt/java/jdk1.4")
+                                             ("1.5" . "/opt/java/jdk1.5")
+                                             ("1.6" . "/opt/java/jdk1.6"))))
      (set-default 'jde-jdk (quote ("1.6")))
      (setq semantic-new-buffer-setup-functions (append '((jde-mode . wisent-java-default-setup)) semantic-new-buffer-setup-functions))))
 
@@ -25,5 +30,6 @@
 
 (eval-after-load 'jde-help
   '(progn
+     (add-to-list 'jde-help-docsets '(nil "http://javadoc.jenkins-ci.org/" nil))
      (add-to-list 'jde-help-docsets '(nil "http://download.oracle.com/docs/cd/E17802_01/j2se/javase/technologies/desktop/javabeans/glasgow/javadocs/" nil)) ;; JavaMail
      (add-to-list 'jde-help-docsets '(nil "http://hadoop.apache.org/common/docs/r0.20.2/api/" nil)))) ;; Hadoop
