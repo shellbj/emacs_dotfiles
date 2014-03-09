d3384ed86797702493b9856f035d7046dc5564d5
Bryan Shell <bryan.shell@orbitz.com>
2014-03-08 18:33:49 -0600

    Convert el-get setup to user/initialize-after-load

--- el-get-init.el
+++ el-get-init.el
@@ -1,6 +1,4 @@
-;; Define where to look for init-pkgname.el configurations
-(setq el-get-user-package-directory
-      (expand-file-name "el-get-init" user-emacs-directory))
+;; el-get bootstrap
 
 ;; use the latest el-get
 (setq el-get-master-branch t)
@@ -8,83 +6,6 @@
 ;; use ssh protocal for install url
 (setq el-get-git-install-url
       "git@github.com:dimitri/el-get.git")
-
-;; use ssh protocal for all github recipes
-(setq el-get-github-default-url-type 'ssh)
-
-;; be verbose about package setup
-;(setq el-get-verbose t)
-
-;; use these recipes
-(setq el-get-packages
-      '(el-get
-        magit
-        gist
-        yasnippet
-        undo-tree
-        diminish
-
-        company-mode
-
-        ;; ido addons
-        ;; ido-ubiquitous
-        smex
-
-        ;; css handling
-        sass-mode
-        scss-mode
-
-        ;; emacs-lisp
-        elisp-slime-nav
-
-        ;; lisp
-        autopair
-        paredit
-        rainbow-delimiters
-
-        ;; puppet
-        puppet-mode
-
-        ;; documentation
-        adoc-mode
-        haml-mode
-        markdown-mode
-        mustache-mode
-        rst-mode
-        yaml-mode
-        jekyll-el
-
-        ;; groovy
-        Emacs-Groovy-Mode
-
-        ;; ruby
-        ruby-mode
-        rspec-mode
-        rvm
-
-        ;; python
-        python-mode
-        pylookup
-        python-pep8
-        ropemacs
-        virtualenv
-        jinja2-mode
-
-        ;; haskell
-        haskell-mode
-        ;; haskell-mode-exts
-
-        ;; clojure
-        clojure-mode
-        cljsbuild-mode
-        midje-mode
-        elein
-        ;; nrepl
-        ;; ac-nrepl
-
-        ;; scala
-        scala-mode
-))
 
 ;; try to require el-get
 (unless (require 'el-get nil 'noerror)
@@ -95,14 +16,3 @@
           el-get-install-skip-emacswiki-recipes)
       (goto-char (point-max))
       (eval-print-last-sexp))))
-
-;; successfully required el-get, load the packages!
-(let ((my-packages (append el-get-packages
-                           (mapcar 'el-get-source-name el-get-sources))))
-	;; Add locally curated recipes for el-get
-  (add-to-list 'el-get-recipe-path
-               (expand-file-name "recipes" user-emacs-directory))
-
-  ;;;; Cleanup has a tendiency to break things; dirty unused checkouts are better
-  ;; (el-get-cleanup my-packages)
-  (el-get 'sync my-packages))
---
+++ el-get.conf.el
@@ -0,0 +1,98 @@
+;; el-get configuration
+
+;; Defer evaluation of :post-init and :after until libraries are required
+;(setq el-get-is-lazy t)
+
+;; Be verbose about package setup
+;(setq el-get-verbose t)
+
+;; Define where to look for init-pkgname.el configurations
+(setq el-get-user-package-directory
+      (expand-file-name "el-get-init" user-emacs-directory))
+
+;; Use the latest el-get
+(setq el-get-master-branch t)
+
+;; Use ssh protocal for all github recipes
+(setq el-get-github-default-url-type 'ssh)
+
+;; Add locally curated recipes for el-get
+(add-to-list 'el-get-recipe-path
+             (expand-file-name "recipes" user-emacs-directory))
+
+;; Locally curated defined inline
+(setq el-get-sources nil)
+
+;; Use these recipes
+(el-get nil
+        '(
+        magit
+        ;; gist
+        yasnippet
+        undo-tree
+        ;; diminish
+
+        fill-column-indicator
+
+        company-mode
+
+        ;; ido addons
+        ;; ido-ubiquitous
+        smex
+
+        ;; css handling
+        sass-mode
+        scss-mode
+
+        ;; emacs-lisp
+        ;;         elisp-slime-nav
+
+        ;; lisp
+        ;;         autopair
+        ;;         paredit
+        ;;         rainbow-delimiters
+
+        ;; puppet
+        ;; ;        puppet-mode
+
+        ;; documentation
+        ;; ;        adoc-mode
+        ;; ;        haml-mode
+        markdown-mode
+        ;; ;        mustache-mode
+        rst-mode
+        yaml-mode
+        textile-mode
+        jekyll-el
+
+        ;; groovy
+        Emacs-Groovy-Mode
+
+        ;; ruby
+        ruby-mode
+        ;;         rspec-mode
+        ;;         rvm
+
+        ;; python
+        python-mode
+        ;;         pylookup
+        ;;         python-pep8
+        ;;         ropemacs
+        ;;         virtualenv
+        ;;         jinja2-mode
+
+        ;; haskell
+        ;;         haskell-mode
+        ;;         haskell-mode-exts
+
+        ;; clojure
+        ;;         clojure-mode
+        ;;         cljsbuild-mode
+        ;;         midje-mode
+        ;;         elein
+        ;;         ;;nrepl
+        ;;         ;;ac-nrepl
+
+        ;; scala
+        ;;         scala-mode
+        ))
