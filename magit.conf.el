88f76d555ad0e56b3bd82dd74a10482ebd776c1e
Bryan Shell <bryan.shell@orbitz.com>
2015-06-05 14:58:23 -0500

    MAGIT: stop with the nagging!

--- magit.conf.el
+++ magit.conf.el
@@ -1,6 +1,9 @@
 ;;;###autoload
 (global-set-key (kbd "C-x C-z") 'magit-status)
 
+;; I know about the buffer reverts, quit nagging me!
+(setq magit-last-seen-setup-instructions "1.4.0")
+
 ;; Enable spell checking, fill for log editing
 (add-hook 'magit-log-edit-mode-hook
           (lambda()
